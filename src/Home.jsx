8a2a07ecdf03265b83ee035ed7c8245577e86a1a
unknown <george.calderon2000@gmail.com>
2025-07-31 18:31:43 -0400

    finished basic styling pokidex homepage formatting

--- src/Home.jsx
+++ src/Home.jsx
@@ -23,23 +23,23 @@ export default function Home () {
     if (isError) return <p>Something went wrong!</p>;
 
     return (
-        <div>
-            <ul>
+        <div className="flex flex-col items-center justify-center min-h-screen gap-y-5">
+            <ul className="grid grid-cols-3 text-blue-700 text-center gap-x-15 gap-y-3 underline">
                 {data.results.map((pokemon) => (
                     <li key={pokemon.name}>
                         <Link to={`/pokemon/${pokemon.name}`}>{pokemon.name}</Link>
                     </li>
                 ))}
             </ul>
-            <div className="flex justify-between pt-4">
+            <div className="flex gap-30">
                 <button onClick={() => setPage((p) => Math.max(p - 1, 0))} disabled={page === 0} 
-                className="px-4 py-2 bg-blue-500 text-white rounded"    
+                className="px-4 py-2 bg-blue-700 text-white rounded"    
                 >Previous</button>
                 <span>
                     Page {page + 1}
                 </span>
                 <button onClick={() => setPage((p) => p + 1)} disabled={!data.next}
-                className="px-4 py-2 bg-blue-500 text-white rounded" 
+                className="px-4 py-2 bg-blue-700 text-white rounded" 
                 >Next</button>
             </div>
         </div>
